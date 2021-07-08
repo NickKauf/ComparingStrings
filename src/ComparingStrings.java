8d7a49ae57ea8d3f1350807067c47c85531880fd
nickkauf <nkauf87@gmail.com>
2021-07-07 21:43:28 -0400

    stormy second try

--- src/ComparingStrings.java
+++ src/ComparingStrings.java
@@ -10,13 +10,16 @@ public class ComparingStrings{
         if (answer.equals("rain")) {
             System.out.println("Take your umbrella!");
         }
-            else if (answer.equals("windy")) {
+        else if (answer.equals("windy")) {
             System.out.println("Wear your jacket!");
         }
-            else if (answer.equals("snow")) {
+        else if (answer.equals("snow")) {
             System.out.println("Wear a coat and take a shovel!");
         }
-            else {
+        else if (answer.equals("stormy")){
+            System.out.println("You should stay inside!");
+        }
+        else {
             System.out.println("Enjoy your day!");
         }
     }
